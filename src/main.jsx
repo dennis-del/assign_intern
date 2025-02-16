97deeb563d120da82f1b0713f35cea9af3db3ab3
dennis-del <dennisjames991@gmail.com>
2025-02-17 00:39:29 +0530

    package.json updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import NumberInput from "./NumberInput";
 import "./styles.css";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <NumberInput />
+    <App />
   </React.StrictMode>
 );
