64ef20cf091a9e07ee2007bba1e940cffd7ffc02
dennis-del <dennisjames991@gmail.com>
2025-02-17 00:23:59 +0530

    main.jsx updated

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // This ensures correct relative pathing for deployment
+  base: '/assign_intern/', // This ensures correct relative pathing for deployment
 });
